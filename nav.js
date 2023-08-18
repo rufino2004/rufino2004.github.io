409296fe027295c084eafb512d4d9fb99ee5c946
rufino2004 <142555774+rufino2004@users.noreply.github.com>
2023-08-17 21:07:51 -0500

    creando mi sitio web

---
+++ nav.js
@@ -0,0 +1,27 @@
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "60%"
+}
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%"
+}
+
+//Botones de carrusel
+
+    //    const btnLeft = document.querySelector(".btn-left"),
+    //        btnRight = document.querySelector(".btn-right"),
+    //        slider = document.querySelector("#slider"),
+    //        sliderSection = document.querySelectorAll("seccion-carrusel")
+    //    btnLeft.addEventListener("click", e => moveToLeft())
+    //    btnRight.addEventListener("click", e => moveToRight())
+
+    //    let operacion = 0;
+    //        widhtImg = 100 / sliderSection.length
+
+    //    function moveToRight() {
+    //        operacion = operacion + 25
+    //        slider.style.transform= `translate(-${operacion}%)`
+    //        slider.style.transition = "all ease .6s"
+    //    }
+    //    function moveToLeft() {
+            
+    //    }
